8aa7d14af35d73cde572896cb9697b1d81a4b8ea
mindo <epsjw1032@naver.com>
2026-01-08 17:36:12 +0900

    headerLink

--- src/main/java/com/example/qwershop/user/item/service/ItemService.java
+++ src/main/java/com/example/qwershop/user/item/service/ItemService.java
@@ -12,7 +12,7 @@ public class ItemService {
     @Autowired
     private ItemMapper itemMapper;
 
-    public List<ItemDto> getItemList(Integer type) {
-        return itemMapper.getItemList(type);
+    public List<ItemDto> getItemList(Integer type, String sort) {
+        return itemMapper.getItemList(type, sort);
     }
 }
