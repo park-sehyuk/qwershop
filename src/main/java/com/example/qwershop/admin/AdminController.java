ae8cfb68d9e8181bb6b45718c0e1cbfc2434dcfa
seungwon <tmddnjs8093@naver.com>
2026-01-13 15:39:42 +0900

    admin user list1

--- src/main/java/com/example/qwershop/admin/AdminController.java
+++ src/main/java/com/example/qwershop/admin/AdminController.java
@@ -1,10 +1,16 @@
 package com.example.qwershop.admin;
 
+import com.example.qwershop.admin.itemManagement.service.CodeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final CodeService codeService;
 
     @GetMapping("/admin/itemList")
     public String itemLlist(){
@@ -16,5 +22,11 @@ public class AdminController {
     public String category(){
         return "admin/category";
     }
+
+//    @GetMapping("/admin/codeList")
+//    public String codeList(Model model) {
+//        model.addAttribute("groupList", codeService.getGroupList());
+//        return "admin/codeList";
+//    }
 
 }
--- src/main/java/com/example/qwershop/admin/itemManagement/mapper/CodeMapper.java
+++ src/main/java/com/example/qwershop/admin/itemManagement/mapper/CodeMapper.java
@@ -2,17 +2,14 @@ package com.example.qwershop.admin.itemManagement.mapper;
 
 import com.example.qwershop.admin.itemManagement.dto.CommonCodeDto;
 import org.apache.ibatis.annotations.Mapper;
-
 import java.util.List;
 
 @Mapper
-public interface ItemManagerMapper {
-
+public interface CodeMapper {
     // 모든 그룹 코드 조회
     List<CommonCodeDto> selectGroupList();
-
+    
     // 특정 그룹에 속한 상세 코드 조회
+    // 디테일 관련 이거 건드림될듯
     List<CommonCodeDto> selectDetailList(String groupCode);
-
-
 }
